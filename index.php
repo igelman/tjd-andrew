3c809b27c7f4f99da8554b6ad490391a4eafa290
igelman <aboutuser@ABT-AI-H03467.local>
2014-06-30 07:49:12 -0400

    Add listing.php
    Refactor coupon grid to listing.php.

--- index.php
+++ index.php
@@ -33,83 +33,8 @@ get_header();
 		<section templ="twoCol">
 			<div class="floatLeft">
 				<?php get_template_part( 'deals' ) ?>
-				<div class="panel panel-default" style="height: 350px;">
-					<div class="panel-heading left quickOffersHeader" id="quickOffersHeader">
-						<h3 class="panel-title"><span>QUICK OFFERS</span><span title="View all your favorites">View all your favorite stores and coupons at a glance</span></h3>
-					</div> <!-- .panel-heading left quickOffersHeader -->
-					<div class="panel-body" style="background-color:#000; padding:0">
-						<div id="quickOffersBodySearch">
-							<div>
-								<div class="floatLeft f15" title="Apparel">APPAREL (247)
-								</div>
-								<div>
-									<label class="sr-only" for="apparelSearch">Apparel Search
-									</label>
-									<input type="text" style="width: 300px" class="form-control f11" id="apparelSearch" placeholder="SEARCH FOR A STORE">
-								</div>
-							</div>
-						</div> <!-- #quickOffersBodySearch -->
-					</div> <!-- .panel-body -->
-					<div id="quickOffersBodyCount">
-						<div class="floatLeft f15">APPAREL (247 Merchants)
-						</div>
-					</div> <!-- #quickOffersBodyCount -->
-					<table id="quickOfferTbl">
-			<thead>
-				<tr>
-					<th>Store</th>
-					<th>Details</th>
-					<th>+Coupon Codes</th>
-					<th>Ending</th>
-					<th style="text-align:center">Go to Store</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td><span>Macys.com</span></td>
-					<td>Lowest Prices of the...</td>
-					<td>2BCGEXX</td>
-					<td>12/14</td>
-					<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-				</tr>
-				<tr>
-					<td><span>Nike.com</span></td>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-					<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-				</tr>
-				<tr>
-					<td><span>Nordstorm.com</span></td>
-					<td>Free Shipping...</td>
-					<td>2BCGEXX</td>
-					<td>12/14</td>
-					<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-				</tr>
-				<tr>
-					<td><span>ShoeBuy.com </span></td>
-					<td>20% Off</td>
-					<td>2BCGEXX</td>
-					<td>12/14</td>
-					<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-				</tr>
-				<tr>
-					<td><span>Kohls.com</span></td>
-					<td>Lowest Prices of the...</td>
-					<td>15% Off</td>
-					<td>12/14</td>
-					<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-				</tr>
-				<tr>
-					<td><span>Sole Society</span></td>
-					<td>25% Off</td>
-					<td>2BCGEXX</td>
-					<td>12/14</td>
-					<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-				</tr>
-			</tbody>
-		</table>
-				</div>
+				<?php get_template_part( 'listing' ) ?>
+
 			</div> <!-- .floatLeft -->
 		</section> <!-- ends the right side (1st column) -->
   <!-- This looks like a stray div closing tag or maybe it should be section closing -->
---
+++ listing.php
@@ -0,0 +1,99 @@
+<div class="panel panel-default" style="height: 350px;">
+	<div class="panel-heading left quickOffersHeader" id="quickOffersHeader">
+		<h3 class="panel-title"><span>QUICK OFFERS</span><span title="View all your favorites">View all your favorite stores and coupons at a glance</span></h3>
+	</div> <!-- .panel-heading left quickOffersHeader -->
+	<div class="panel-body" style="background-color:#000; padding:0">
+		<div id="quickOffersBodySearch">
+			<div>
+				<div class="floatLeft f15" title="Apparel">
+					APPAREL (247)
+				</div>
+				<div>
+					<label class="sr-only" for="apparelSearch">Apparel Search</label>
+					<input type="text" style="width: 300px" class="form-control f11" id="apparelSearch" placeholder="SEARCH FOR A STORE">
+				</div>
+			</div>
+		</div> <!-- #quickOffersBodySearch -->
+	</div> <!-- .panel-body -->
+	<div id="quickOffersBodyCount">
+		<div class="floatLeft f15">
+			APPAREL (247 Merchants)
+		</div>
+	</div> <!-- #quickOffersBodyCount -->
+	<table id="quickOfferTbl">
+		<thead>
+			<tr>
+				<th>Store</th>
+				<th>Details</th>
+				<th>+Coupon Codes</th>
+				<th>Ending</th>
+				<th style="text-align:center">Go to Store</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td><span>Macys.com</span></td>
+				<td>Lowest Prices of the...</td>
+				<td>2BCGEXX</td>
+				<td>12/14</td>
+				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
+			</tr>
+			<tr>
+				<td><span>Macys.com</span></td>
+				<td>Lowest Prices of the...</td>
+				<td>2BCGEXX</td>
+				<td>12/14</td>
+				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
+			</tr>
+			<tr>
+				<td><span>Macys.com</span></td>
+				<td>Lowest Prices of the...</td>
+				<td>2BCGEXX</td>
+				<td>12/14</td>
+				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
+			</tr>
+			<tr>
+				<td><span>Macys.com</span></td>
+				<td>Lowest Prices of the...</td>
+				<td>2BCGEXX</td>
+				<td>12/14</td>
+				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
+			</tr>
+			<tr>
+				<td><span>Macys.com</span></td>
+				<td>Lowest Prices of the...</td>
+				<td>2BCGEXX</td>
+				<td>12/14</td>
+				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
+			</tr>
+			<tr>
+				<td><span>Macys.com</span></td>
+				<td>Lowest Prices of the...</td>
+				<td>2BCGEXX</td>
+				<td>12/14</td>
+				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
+			</tr>
+			<tr>
+				<td><span>Macys.com</span></td>
+				<td>Lowest Prices of the...</td>
+				<td>2BCGEXX</td>
+				<td>12/14</td>
+				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
+			</tr>
+			<tr>
+				<td><span>Macys.com</span></td>
+				<td>Lowest Prices of the...</td>
+				<td>2BCGEXX</td>
+				<td>12/14</td>
+				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
+			</tr>
+			<tr>
+				<td><span>Macys.com</span></td>
+				<td>Lowest Prices of the...</td>
+				<td>2BCGEXX</td>
+				<td>12/14</td>
+				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
