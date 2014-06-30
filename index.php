11213b1f7c33a77ae61fd00eb95aad0e2f9f7d0e
igelman <aboutuser@ABT-AI-H03467.local>
2014-06-30 07:56:40 -0400

    Add submit-tip.php
    Refactor submit-a-tip form to submit-tip.php

--- index.php
+++ index.php
@@ -43,27 +43,10 @@ get_header();
 	   
 		<?php get_template_part( 'email-signup') ?>
 		<?php get_template_part( 'expiring-soon') ?>
+		<?php get_template_part( 'submit-tip') ?>
 	   
 	   
-	   <div id="greatTip" class="panel panel-default shorterPanel floatRight left">
-	     <div class="panel-heading">
-			 <h3 class="panel-title">SUBMIT A GREAT TIP</h3>
-		 </div>
-		 <div class="panel-body">
-		    <div title="Send us a tip or coupon">Send us a tip or coupon and help others save!</div>
-		 </div>
-	     <p class="searchField center">
-	       <label class="sr-only" for="enterCode">Enter Code</label>
-	       <input type="text" class="form-control f11" id="enterCode" placeholder="Enter Code">
-	     </p>
-	     <p class="searchField center">
-	       <label class="sr-only" for="enterCodeDesc">Enter Discount Description</label>
-	       <textarea rows="4" class="form-control f11" id="enterCodeDesc" placeholder="Enter discount description"></textarea>
-	     </p>
-		 <p class="center">
-			 <button class="btn btn-lg btn-success">Submit this tip</button>
-	     </p>
-	   </div>
+
 
 	   <div id="popularDeals" class="panel panel-default shorterPanel floatRight left">
 		 <div class="panel-heading">
---
+++ submit-tip.php
@@ -0,0 +1,20 @@
+<div id="greatTip" class="panel panel-default shorterPanel floatRight left">
+	<div class="panel-heading">
+		<h3 class="panel-title">SUBMIT A GREAT TIP</h3>
+	</div>
+	<div class="panel-body">
+		<div title="Send us a tip or coupon">Send us a tip or coupon and help others save!
+		</div>
+	</div>
+	<p class="searchField center">
+		<label class="sr-only" for="enterCode">Enter Code</label>
+		<input type="text" class="form-control f11" id="enterCode" placeholder="Enter Code">
+	</p>
+	<p class="searchField center">
+		<label class="sr-only" for="enterCodeDesc">Enter Discount Description</label>
+		<textarea rows="4" class="form-control f11" id="enterCodeDesc" placeholder="Enter discount description"></textarea>
+	</p>
+	<p class="center">
+		<button class="btn btn-lg btn-success">Submit this tip</button>
+	</p>
+</div>
