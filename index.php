837b0757b421c7c85c27e41df4d4dac738edd4e3
igelman <aboutuser@ABT-AI-H03467.local>
2014-06-29 21:01:58 -0400

    Add footer-content.php.
    Refactor footer element to footer.php.

--- index.php
+++ index.php
@@ -536,41 +536,8 @@ get_template_part( 'navbar' );
 	 </div>
 	</div> 
 	
-	<footer>
-		<table>
-			<tr line="first">
-				<td colspan="5">&nbsp;</td>
-			</tr>
-			<tr line="second">
-				<td title="bird"><img src="images/bird.png"></td>
-				<td align="left"><ABOUT TIPJARDAILY<br>
-					<span title="Welcome to TipJarDaily">Welcome to TipjarDaily.</span><br>
-					<span title="Our Mission">Our Mission is to track great deals, coupons and your favorite stores... (<a href="#">read more.</a>)</span>
-				</td>
-				<td align="left" valign="top">
-					<h4>CONTACT US</h4>
-					<div title="Customer Support">Customer Support</span>
-				</td>
-				<td align="left" valign="top">
-					<h4>ABOUT US</h4>
-					<div>Meet the team</div>
-					<div>Customer Support</div>
-				</td>
-				<td align="left" valign="top">
-					<h4>CONNECT WITH US</h4>
-					<div>Facebook</div>
-					<div>Twitter</div>
-					<div>Pinterest</div>
-				</td>
-			</tr>
-			<tr line="third">
-				<td colspan="5">&nbsp;</td>
-			</tr>
-		</table>
-		<div copyright="last">
-			Copyright @copy 2014 TipJarDaily, All Rights Reserved
-		</div>
-	 </footer>
+	<?php get_template_part( 'footer-content') ?>
+
 	</div>
    </section>
 
