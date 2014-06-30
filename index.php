2bbeabe4eedb6b6743b9c646481546799f731684
igelman <aboutuser@ABT-AI-H03467.local>
2014-06-29 21:05:50 -0400

    Add navbar.php.
    Refactor header element and navbar div to navbar.php.

--- index.php
+++ index.php
@@ -28,60 +28,7 @@ get_template_part( 'navbar' );
 
 	  
     <section templ="wrapper">
-		
-	   <header>
-		  <img src="<?php bloginfo('template_directory') ?>/assets/images/tipjardaily.png" title="TipJar Daily" alt="Logo">	  
-		  <div id="searchBox">
-			  <div class="searchField">
-				  <table>
-					  <tr>
-						  <td>
-			  			    <label class="sr-only" for="searchMerchants">Search Merchants</label>
-			  			    <input type="text" class="form-control f11" id="searchMerchants" placeholder="Search merchants by name (eq.sephora hp)">
-						  </td>
-						  <td class="ml10">
-							  <button title="Browse Coupons" class="btn">Browse Coupons</button>
-						  </td>
-					  </tr>
-					  <tr>
-						  <td class="pt3">
-			  			    <label class="sr-only" for="subscribeEmail">Search Merchants</label>
-			  			    <input type="text" class="form-control" id="subscribeEmail" placeholder="Subscribe via Email">
-						  </td>
-						  <td class="pt3">
-							  <button title="Browse Coupons" class="btn">Subscribe</button>
-						  </td>
-					  </tr>
-				  </table>	
-			  </div>		  		 
-	      </div>
-	   </header>
-	   		
-       <div class="navbar navbar-inverse" role="navigation">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-          </div>
-          <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-			  <li><a href="#">HOME <span>&#124;</span></a></li>
-              <li><a href="#">WOMEN'S <span>&#124;</span></a></li>
-              <li><a href="#">MEN'S <span>&#124;</span></a></li>
-              <li><a href="#">KID'S <span>&#124;</span></a></li>
-			  <li><a href="#">FREE + IN-STORE <span>&#124;</span></a></li>
-			  <li><a href="#">BEAUTY + HEALTH <span>&#124;</span></a></li>
-			  <li><a href="#">ELECTRONICS <span>&#124;</span></a></li>
-			  <li><a href="#">FOOD + GIFTS <span>&#124;</span></a></li>
-			  <li><a href="#">DEAL-OF-THE-DAY</a></li>
-            </ul>       
-          </div>
-        </div>
-       </div>   
+		<?php get_template_part( 'navbar') ?>	
 
 	   <div class="well">
 		  <div promotion="latest">		  	
