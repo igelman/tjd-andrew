b4baaead32449ad7e7fdcdd8c366d61b693ac5e5
igelman <aboutuser@ABT-AI-H03467.local>
2014-06-29 21:48:02 -0400

    Clean up deals.php
    Clean up div.latestDealsBox mark-up in deals.php.

--- deals.php
+++ deals.php
@@ -3,124 +3,266 @@
 		<h3 style="text-align: left" class="panel-title">LATEST DEALS</h3>
 	</div> <!-- .panel-heading -->
 	<div class="panel-body">
-			 <div class="latestDealsBox"> <!-- start b -->
-				 <div class="floatLeft pb10 mr10 two200by200">
-					 <img src="<?php bloginfo('template_directory') ?>/assets/images/shoe.png">
-					 <div class="f11 ctr80">
-						 Margot mid-wedge elastic espadrilles,<b>$19</b>
-					 </div>
+
+
+
+		<div class="latestDealsBox"> <!-- start b -->
+			<div class="floatLeft pb10 mr10 two200by200">
+				<img src="<?php bloginfo('template_directory') ?>/assets/images/shoe.png">
+				<div class="f11 ctr80">
+					Margot mid-wedge elastic espadrilles,<b>$19</b>
 				</div>
-				<div class="left pb10"> <!-- a -->
-					<div caption="sale">Macy's <span>20% off</span> All Items coupon (+ Free Shipping on $99), <span>25% off</span> Sitewide coupon (+ Clearance Sale, deals from <span>$3</span>)</div>
-					<p code="sale-flower20">
-						<span class="blue">CODE:</span><span class="orange">FLOWER20</span>
-						<span class="blue">PIN:</span><span class="orange">1490</span>
-						<span>(EXP.3/31)</span>
-					</p>
-					<div sale="props-headliner">
-						Stock up on stylish essentials for a steal with the Warehouse Event at Land's End, down jackets, vests, shoes and more - plus free shipping on $50. Some picks:
-					</div>
-					<ul sale="props-cat">
-						<li>Women's Tops, Cardigans (from <b>$4</b>)</li>
-						<li>Snoes, Clogs, Mocs (from <b>$19</b>)</li>
-						<li>Men's Shirts (from <b>$6</b>)</li>
-						<li>Men's Pants (from <b>$12</b>)</li>
-						<li><b>View all items &gt;</b></li>
-					</ul>
-					<p title="View More">
-						View more in <a class="more" href="#">Apparel</a>, <a href="#">Gifts</a>, <a href="#">Jewelry</a> 
-					</p>
-				</div>	<!-- ends a -->
-			 </div> <!-- ends b -->
-			 			 
-			 <div class="latestDealsBox"> <!-- start b -->
-				 <div class="floatLeft pb10 mr10 two200by200">
-					 <img src="<?php bloginfo('template_directory') ?>/assets/images/shoe.png">
-					 <div class="f11 ctr80">
-						 Margot mid-wedge elastic espadrilles,<b>$19</b>
-					 </div>
+			</div>
+			
+			<div class="left pb10"> <!-- a -->
+				<div caption="sale">Macy's <span>20% off</span> All Items coupon (+ Free Shipping on $99), <span>25% off</span> Sitewide coupon (+ Clearance Sale, deals from <span>$3</span>)
 				</div>
-				<div class="left pb10"> <!-- a -->
-					<div caption="sale">Macy's <span>20% off</span> All Items coupon (+ Free Shipping on $99), <span>25% off</span> Sitewide coupon (+ Clearance Sale, deals from <span>$3</span>)</div>
-					<p code="sale-flower20">
-						<span class="blue">CODE:</span><span class="orange">FLOWER20</span>
-						<span class="blue">PIN:</span><span class="orange">1490</span>
-						<span>(EXP.3/31)</span>
-					</p>
-					<div sale="props-headliner">
-						Stock up on stylish essentials for a steal with the Warehouse Event at Land's End, down jackets, vests, shoes and more - plus free shipping on $50. Some picks:
-					</div>
-					<ul sale="props-cat">
-						<li>Women's Tops, Cardigans (from <b>$4</b>)</li>
-						<li>Snoes, Clogs, Mocs (from <b>$19</b>)</li>
-						<li>Men's Shirts (from <b>$6</b>)</li>
-						<li>Men's Pants (from <b>$12</b>)</li>
-						<li><b>View all items &gt;</b></li>
-					</ul>
-					<p title="View More">
-						View more in <a class="more" href="#">Apparel</a>, <a href="#">Gifts</a>, <a href="#">Jewelry</a> 
-					</p>
-				</div>	<!-- ends a -->
-			 </div> <!-- ends b -->
+				<p code="sale-flower20">
+					<span class="blue">CODE:</span><span class="orange">FLOWER20</span>
+					<span class="blue">PIN:</span><span class="orange">1490</span>
+					<span>(EXP.3/31)</span>
+				</p>
+				<div sale="props-headliner">
+					Stock up on stylish essentials for a steal with the Warehouse Event at Land's End, down jackets, vests, shoes and more - plus free shipping on $50. Some picks:
+				</div>
+				<ul sale="props-cat">
+					<li>Women's Tops, Cardigans (from <b>$4</b>)</li>
+					<li>Snoes, Clogs, Mocs (from <b>$19</b>)</li>
+					<li>Men's Shirts (from <b>$6</b>)</li>
+					<li>Men's Pants (from <b>$12</b>)</li>
+					<li><b>View all items &gt;</b></li>
+				</ul> <!-- sale="props-cat" -->
+				<p title="View More">
+					View more in <a class="more" href="#">Apparel</a>, <a href="#">Gifts</a>, <a href="#">Jewelry</a>
+				</p>
+			</div>	<!-- ends a -->
+
+		</div> <!-- .latestDealsBox -->
+		<div class="latestDealsBox"> <!-- start b -->
+			<div class="floatLeft pb10 mr10 two200by200">
+				<img src="<?php bloginfo('template_directory') ?>/assets/images/shoe.png">
+				<div class="f11 ctr80">
+					Margot mid-wedge elastic espadrilles,<b>$19</b>
+				</div>
+			</div>
+			
+			<div class="left pb10"> <!-- a -->
+				<div caption="sale">Macy's <span>20% off</span> All Items coupon (+ Free Shipping on $99), <span>25% off</span> Sitewide coupon (+ Clearance Sale, deals from <span>$3</span>)
+				</div>
+				<p code="sale-flower20">
+					<span class="blue">CODE:</span><span class="orange">FLOWER20</span>
+					<span class="blue">PIN:</span><span class="orange">1490</span>
+					<span>(EXP.3/31)</span>
+				</p>
+				<div sale="props-headliner">
+					Stock up on stylish essentials for a steal with the Warehouse Event at Land's End, down jackets, vests, shoes and more - plus free shipping on $50. Some picks:
+				</div>
+				<ul sale="props-cat">
+					<li>Women's Tops, Cardigans (from <b>$4</b>)</li>
+					<li>Snoes, Clogs, Mocs (from <b>$19</b>)</li>
+					<li>Men's Shirts (from <b>$6</b>)</li>
+					<li>Men's Pants (from <b>$12</b>)</li>
+					<li><b>View all items &gt;</b></li>
+				</ul> <!-- sale="props-cat" -->
+				<p title="View More">
+					View more in <a class="more" href="#">Apparel</a>, <a href="#">Gifts</a>, <a href="#">Jewelry</a>
+				</p>
+			</div>	<!-- ends a -->
+
+		</div> <!-- .latestDealsBox -->
+		<div class="latestDealsBox"> <!-- start b -->
+			<div class="floatLeft pb10 mr10 two200by200">
+				<img src="<?php bloginfo('template_directory') ?>/assets/images/shoe.png">
+				<div class="f11 ctr80">
+					Margot mid-wedge elastic espadrilles,<b>$19</b>
+				</div>
+			</div>
+			
+			<div class="left pb10"> <!-- a -->
+				<div caption="sale">Macy's <span>20% off</span> All Items coupon (+ Free Shipping on $99), <span>25% off</span> Sitewide coupon (+ Clearance Sale, deals from <span>$3</span>)
+				</div>
+				<p code="sale-flower20">
+					<span class="blue">CODE:</span><span class="orange">FLOWER20</span>
+					<span class="blue">PIN:</span><span class="orange">1490</span>
+					<span>(EXP.3/31)</span>
+				</p>
+				<div sale="props-headliner">
+					Stock up on stylish essentials for a steal with the Warehouse Event at Land's End, down jackets, vests, shoes and more - plus free shipping on $50. Some picks:
+				</div>
+				<ul sale="props-cat">
+					<li>Women's Tops, Cardigans (from <b>$4</b>)</li>
+					<li>Snoes, Clogs, Mocs (from <b>$19</b>)</li>
+					<li>Men's Shirts (from <b>$6</b>)</li>
+					<li>Men's Pants (from <b>$12</b>)</li>
+					<li><b>View all items &gt;</b></li>
+				</ul> <!-- sale="props-cat" -->
+				<p title="View More">
+					View more in <a class="more" href="#">Apparel</a>, <a href="#">Gifts</a>, <a href="#">Jewelry</a>
+				</p>
+			</div>	<!-- ends a -->
+
+		</div> <!-- .latestDealsBox -->
+		<div class="latestDealsBox"> <!-- start b -->
+			<div class="floatLeft pb10 mr10 two200by200">
+				<img src="<?php bloginfo('template_directory') ?>/assets/images/shoe.png">
+				<div class="f11 ctr80">
+					Margot mid-wedge elastic espadrilles,<b>$19</b>
+				</div>
+			</div>
+			
+			<div class="left pb10"> <!-- a -->
+				<div caption="sale">Macy's <span>20% off</span> All Items coupon (+ Free Shipping on $99), <span>25% off</span> Sitewide coupon (+ Clearance Sale, deals from <span>$3</span>)
+				</div>
+				<p code="sale-flower20">
+					<span class="blue">CODE:</span><span class="orange">FLOWER20</span>
+					<span class="blue">PIN:</span><span class="orange">1490</span>
+					<span>(EXP.3/31)</span>
+				</p>
+				<div sale="props-headliner">
+					Stock up on stylish essentials for a steal with the Warehouse Event at Land's End, down jackets, vests, shoes and more - plus free shipping on $50. Some picks:
+				</div>
+				<ul sale="props-cat">
+					<li>Women's Tops, Cardigans (from <b>$4</b>)</li>
+					<li>Snoes, Clogs, Mocs (from <b>$19</b>)</li>
+					<li>Men's Shirts (from <b>$6</b>)</li>
+					<li>Men's Pants (from <b>$12</b>)</li>
+					<li><b>View all items &gt;</b></li>
+				</ul> <!-- sale="props-cat" -->
+				<p title="View More">
+					View more in <a class="more" href="#">Apparel</a>, <a href="#">Gifts</a>, <a href="#">Jewelry</a>
+				</p>
+			</div>	<!-- ends a -->
+
+		</div> <!-- .latestDealsBox -->
+		<div class="latestDealsBox"> <!-- start b -->
+			<div class="floatLeft pb10 mr10 two200by200">
+				<img src="<?php bloginfo('template_directory') ?>/assets/images/shoe.png">
+				<div class="f11 ctr80">
+					Margot mid-wedge elastic espadrilles,<b>$19</b>
+				</div>
+			</div>
+			
+			<div class="left pb10"> <!-- a -->
+				<div caption="sale">Macy's <span>20% off</span> All Items coupon (+ Free Shipping on $99), <span>25% off</span> Sitewide coupon (+ Clearance Sale, deals from <span>$3</span>)
+				</div>
+				<p code="sale-flower20">
+					<span class="blue">CODE:</span><span class="orange">FLOWER20</span>
+					<span class="blue">PIN:</span><span class="orange">1490</span>
+					<span>(EXP.3/31)</span>
+				</p>
+				<div sale="props-headliner">
+					Stock up on stylish essentials for a steal with the Warehouse Event at Land's End, down jackets, vests, shoes and more - plus free shipping on $50. Some picks:
+				</div>
+				<ul sale="props-cat">
+					<li>Women's Tops, Cardigans (from <b>$4</b>)</li>
+					<li>Snoes, Clogs, Mocs (from <b>$19</b>)</li>
+					<li>Men's Shirts (from <b>$6</b>)</li>
+					<li>Men's Pants (from <b>$12</b>)</li>
+					<li><b>View all items &gt;</b></li>
+				</ul> <!-- sale="props-cat" -->
+				<p title="View More">
+					View more in <a class="more" href="#">Apparel</a>, <a href="#">Gifts</a>, <a href="#">Jewelry</a>
+				</p>
+			</div>	<!-- ends a -->
+
+		</div> <!-- .latestDealsBox -->
+		<div class="latestDealsBox"> <!-- start b -->
+			<div class="floatLeft pb10 mr10 two200by200">
+				<img src="<?php bloginfo('template_directory') ?>/assets/images/shoe.png">
+				<div class="f11 ctr80">
+					Margot mid-wedge elastic espadrilles,<b>$19</b>
+				</div>
+			</div>
+			
+			<div class="left pb10"> <!-- a -->
+				<div caption="sale">Macy's <span>20% off</span> All Items coupon (+ Free Shipping on $99), <span>25% off</span> Sitewide coupon (+ Clearance Sale, deals from <span>$3</span>)
+				</div>
+				<p code="sale-flower20">
+					<span class="blue">CODE:</span><span class="orange">FLOWER20</span>
+					<span class="blue">PIN:</span><span class="orange">1490</span>
+					<span>(EXP.3/31)</span>
+				</p>
+				<div sale="props-headliner">
+					Stock up on stylish essentials for a steal with the Warehouse Event at Land's End, down jackets, vests, shoes and more - plus free shipping on $50. Some picks:
+				</div>
+				<ul sale="props-cat">
+					<li>Women's Tops, Cardigans (from <b>$4</b>)</li>
+					<li>Snoes, Clogs, Mocs (from <b>$19</b>)</li>
+					<li>Men's Shirts (from <b>$6</b>)</li>
+					<li>Men's Pants (from <b>$12</b>)</li>
+					<li><b>View all items &gt;</b></li>
+				</ul> <!-- sale="props-cat" -->
+				<p title="View More">
+					View more in <a class="more" href="#">Apparel</a>, <a href="#">Gifts</a>, <a href="#">Jewelry</a>
+				</p>
+			</div>	<!-- ends a -->
+
+		</div> <!-- .latestDealsBox -->
+		<div class="latestDealsBox"> <!-- start b -->
+			<div class="floatLeft pb10 mr10 two200by200">
+				<img src="<?php bloginfo('template_directory') ?>/assets/images/shoe.png">
+				<div class="f11 ctr80">
+					Margot mid-wedge elastic espadrilles,<b>$19</b>
+				</div>
+			</div>
+			
+			<div class="left pb10"> <!-- a -->
+				<div caption="sale">Macy's <span>20% off</span> All Items coupon (+ Free Shipping on $99), <span>25% off</span> Sitewide coupon (+ Clearance Sale, deals from <span>$3</span>)
+				</div>
+				<p code="sale-flower20">
+					<span class="blue">CODE:</span><span class="orange">FLOWER20</span>
+					<span class="blue">PIN:</span><span class="orange">1490</span>
+					<span>(EXP.3/31)</span>
+				</p>
+				<div sale="props-headliner">
+					Stock up on stylish essentials for a steal with the Warehouse Event at Land's End, down jackets, vests, shoes and more - plus free shipping on $50. Some picks:
+				</div>
+				<ul sale="props-cat">
+					<li>Women's Tops, Cardigans (from <b>$4</b>)</li>
+					<li>Snoes, Clogs, Mocs (from <b>$19</b>)</li>
+					<li>Men's Shirts (from <b>$6</b>)</li>
+					<li>Men's Pants (from <b>$12</b>)</li>
+					<li><b>View all items &gt;</b></li>
+				</ul> <!-- sale="props-cat" -->
+				<p title="View More">
+					View more in <a class="more" href="#">Apparel</a>, <a href="#">Gifts</a>, <a href="#">Jewelry</a>
+				</p>
+			</div>	<!-- ends a -->
+
+		</div> <!-- .latestDealsBox -->
+		<div class="latestDealsBox"> <!-- start b -->
+			<div class="floatLeft pb10 mr10 two200by200">
+				<img src="<?php bloginfo('template_directory') ?>/assets/images/shoe.png">
+				<div class="f11 ctr80">
+					Margot mid-wedge elastic espadrilles,<b>$19</b>
+				</div>
+			</div>
+			
+			<div class="left pb10"> <!-- a -->
+				<div caption="sale">Macy's <span>20% off</span> All Items coupon (+ Free Shipping on $99), <span>25% off</span> Sitewide coupon (+ Clearance Sale, deals from <span>$3</span>)
+				</div>
+				<p code="sale-flower20">
+					<span class="blue">CODE:</span><span class="orange">FLOWER20</span>
+					<span class="blue">PIN:</span><span class="orange">1490</span>
+					<span>(EXP.3/31)</span>
+				</p>
+				<div sale="props-headliner">
+					Stock up on stylish essentials for a steal with the Warehouse Event at Land's End, down jackets, vests, shoes and more - plus free shipping on $50. Some picks:
+				</div>
+				<ul sale="props-cat">
+					<li>Women's Tops, Cardigans (from <b>$4</b>)</li>
+					<li>Snoes, Clogs, Mocs (from <b>$19</b>)</li>
+					<li>Men's Shirts (from <b>$6</b>)</li>
+					<li>Men's Pants (from <b>$12</b>)</li>
+					<li><b>View all items &gt;</b></li>
+				</ul> <!-- sale="props-cat" -->
+				<p title="View More">
+					View more in <a class="more" href="#">Apparel</a>, <a href="#">Gifts</a>, <a href="#">Jewelry</a>
+				</p>
+			</div>	<!-- ends a -->
+
+		</div> <!-- .latestDealsBox -->
 			 
-			 <div class="latestDealsBox"> <!-- start b -->
-				 <div class="floatLeft pb10 mr10 two200by200">
-					 <img src="<?php bloginfo('template_directory') ?>/assets/images/shoe.png">
-					 <div class="f11 ctr80">
-						 Margot mid-wedge elastic espadrilles,<b>$19</b>
-					 </div>
-				</div>
-				<div class="left pb10"> <!-- a -->
-					<div caption="sale">Macy's <span>20% off</span> All Items coupon (+ Free Shipping on $99), <span>25% off</span> Sitewide coupon (+ Clearance Sale, deals from <span>$3</span>)</div>
-					<p code="sale-flower20">
-						<span class="blue">CODE:</span><span class="orange">FLOWER20</span>
-						<span class="blue">PIN:</span><span class="orange">1490</span>
-						<span>(EXP.3/31)</span>
-					</p>
-					<div sale="props-headliner">
-						Stock up on stylish essentials for a steal with the Warehouse Event at Land's End, down jackets, vests, shoes and more - plus free shipping on $50. Some picks:
-					</div>
-					<ul sale="props-cat">
-						<li>Women's Tops, Cardigans (from <b>$4</b>)</li>
-						<li>Snoes, Clogs, Mocs (from <b>$19</b>)</li>
-						<li>Men's Shirts (from <b>$6</b>)</li>
-						<li>Men's Pants (from <b>$12</b>)</li>
-						<li><b>View all items &gt;</b></li>
-					</ul>
-					<p title="View More">
-						View more in <a class="more" href="#">Apparel</a>, <a href="#">Gifts</a>, <a href="#">Jewelry</a> 
-					</p>
-				</div>	<!-- ends a -->
-			 </div> <!-- ends b -->
 			 
-			 <div class="latestDealsBox"> <!-- start b -->
-				 <div class="floatLeft pb10 mr10 two200by200">
-					 <img src="<?php bloginfo('template_directory') ?>/assets/images/shoe.png">
-					 <div class="f11 ctr80">
-						 Margot mid-wedge elastic espadrilles,<b>$19</b>
-					 </div>
-				</div>
-				<div class="left pb10"> <!-- a -->
-					<div caption="sale">Macy's <span>20% off</span> All Items coupon (+ Free Shipping on $99), <span>25% off</span> Sitewide coupon (+ Clearance Sale, deals from <span>$3</span>)</div>
-					<p code="sale-flower20">
-						<span class="blue">CODE:</span><span class="orange">FLOWER20</span>
-						<span class="blue">PIN:</span><span class="orange">1490</span>
-						<span>(EXP.3/31)</span>
-					</p>
-					<div sale="props-headliner">
-						Stock up on stylish essentials for a steal with the Warehouse Event at Land's End, down jackets, vests, shoes and more - plus free shipping on $50. Some picks:
-					</div>
-					<ul sale="props-cat">
-						<li>Women's Tops, Cardigans (from <b>$4</b>)</li>
-						<li>Snoes, Clogs, Mocs (from <b>$19</b>)</li>
-						<li>Men's Shirts (from <b>$6</b>)</li>
-						<li>Men's Pants (from <b>$12</b>)</li>
-						<li><b>View all items &gt;</b></li>
-					</ul>
-					<p title="View More">
-						View more in <a class="more" href="#">Apparel</a>, <a href="#">Gifts</a>, <a href="#">Jewelry</a> 
-					</p>
-				</div>	<!-- ends a -->
-			 </div> <!-- ends b -->
 	</div> <!-- .panel-body -->
 </div> <!-- ends d -->
