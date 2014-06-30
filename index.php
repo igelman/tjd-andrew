a3b1e693ad5763d5a75c78cf4e7d73489279f992
igelman <aboutuser@ABT-AI-H03467.local>
2014-06-29 21:10:35 -0400

    Add tiles-top.php.
    Refactor tiles to tiles-top.php.
    Remove extra call to navbar template.

--- index.php
+++ index.php
@@ -23,50 +23,14 @@
  * )
 */ 
 get_header();
-get_template_part( 'navbar' );
 ?>
 
 	  
     <section templ="wrapper">
-		<?php get_template_part( 'navbar') ?>	
+		<?php get_template_part( 'navbar') ?>
+		<?php get_template_part( 'tiles-top') ?>
 
-	   <div class="well">
-		  <div promotion="latest">		  	
-		    <div promo="2014">
-			   <span class="f18">STYLE: WOMEN'S APPAREL, SHOES &amp; ACCESSORIES</span><span> UPDATED IN THE PAST 24 HOURS</span>
-			</div>		
-		  </div>		
-		  <div id="featuredBoxes">			   
-				   <section title="Featuring Amazon">
-					   <img src="images/amazon.png">
-					   <p>
-					   <cite title="Featured Deal">100 MP3 Albums $5 each</cite>
-					   (Josh Turner, Maroon 5, Jay Z, The Killers and more)
-				       </p>
-				   </section>
-				   <section title="Featuring Shoes">
-					   <img src="images/shoes.png">
-					   <p>
-					   <cite title="Featured Deal">100 MP3 Albums $5 each</cite>
-					   (Josh Turner, Maroon 5, Jay Z, The Killers and more)
-				       </p>
-				   </section>
-				   <section title="Featuring Kohls">
-					   <img src="images/kohls.png">
-					   <p>
-					   <cite title="Featured Deal">100 MP3 Albums $5 each</cite>
-					   (Josh Turner, Maroon 5, Jay Z, The Killers and more)
-				       </p>
-				   </section>
-				   <section title="Redbox" title="Featured Deal">
-					   <img src="images/redbox.png">
-					   <p style="margin-top: 10px">
-					   <cite title="Featured Deal">100 MP3 Albums $5 each</cite>
-					   (Josh Turner, Maroon 5, Jay Z, The Killers and more)
-				       </p>
-				   </section>				
-		    </div>
-	  </div>
+	   
 	    
 	  <section templ="twoCol">
 	  
