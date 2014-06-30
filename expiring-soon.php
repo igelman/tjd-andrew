64c5b1efc51ee58126cc74128ea6f7b36a5526bd
igelman <aboutuser@ABT-AI-H03467.local>
2014-06-29 22:17:27 -0400

    Add expiring-soon.php
    Refactor expiring deals to expiring-soon.php.

---
+++ expiring-soon.php
@@ -0,0 +1,109 @@
+<div class="panel panel-default shorterPanel floatRight">
+	<div class="panel-heading left">
+		<h3 class="panel-title" id="more_exp_soon">MORE EXPIRING SOON</h3>
+	</div>
+	
+	<div class="panel-body left" title="Today and This Week">
+		<div class="f18 mb15">Today and this week!
+		</div>
+		
+		<article title="expiring soon">
+			<div class="floatLeft">
+				<img src="images/star.png">
+			</div>
+			<div class="expBox">
+				<div>
+					Land's End: 25% off + Free Shipping
+				</div>
+				<div>
+					<span class="blue">CODE:</span>
+					<span class="orange">XCYCC</span>
+					<span>(Exp.6/25)</span>
+				</div>
+			</div>
+		</article>
+
+		<article title="expiring soon">
+			<div class="floatLeft">
+				<img src="images/star.png">
+			</div>
+			<div class="expBox">
+				<div>
+					Land's End: 25% off + Free Shipping
+				</div>
+				<div>
+					<span class="blue">CODE:</span>
+					<span class="orange">XCYCC</span>
+					<span>(Exp.6/25)</span>
+				</div>
+			</div>
+		</article>
+
+		<article title="expiring soon">
+			<div class="floatLeft">
+				<img src="images/star.png">
+			</div>
+			<div class="expBox">
+				<div>
+					Land's End: 25% off + Free Shipping
+				</div>
+				<div>
+					<span class="blue">CODE:</span>
+					<span class="orange">XCYCC</span>
+					<span>(Exp.6/25)</span>
+				</div>
+			</div>
+		</article>
+
+		<article title="expiring soon">
+			<div class="floatLeft">
+				<img src="images/star.png">
+			</div>
+			<div class="expBox">
+				<div>
+					Land's End: 25% off + Free Shipping
+				</div>
+				<div>
+					<span class="blue">CODE:</span>
+					<span class="orange">XCYCC</span>
+					<span>(Exp.6/25)</span>
+				</div>
+			</div>
+		</article>
+
+		<article title="expiring soon">
+			<div class="floatLeft">
+				<img src="images/star.png">
+			</div>
+			<div class="expBox">
+				<div>
+					Land's End: 25% off + Free Shipping
+				</div>
+				<div>
+					<span class="blue">CODE:</span>
+					<span class="orange">XCYCC</span>
+					<span>(Exp.6/25)</span>
+				</div>
+			</div>
+		</article>
+
+		<article title="expiring soon">
+			<div class="floatLeft">
+				<img src="images/star.png">
+			</div>
+			<div class="expBox">
+				<div>
+					Land's End: 25% off + Free Shipping
+				</div>
+				<div>
+					<span class="blue">CODE:</span>
+					<span class="orange">XCYCC</span>
+					<span>(Exp.6/25)</span>
+				</div>
+			</div>
+		</article>
+
+		<!-- Last article needs  style="border-bottom: 0 -->	
+			
+		 </div>
+	   </div>  
