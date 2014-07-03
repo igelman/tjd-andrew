336d05a9d71a320d55c9d0adcf91507f85cf54a3
igelman <aboutuser@ABT-AI-H03467.local>
2014-07-02 23:00:35 -0400

    Refactor listing.php
    Generate table rows in WP loop.

--- listing.php
+++ listing.php
@@ -1,3 +1,44 @@
+<?php
+/**
+* Tool to generate query: http://generatewp.com/wp_query/
+*
+* Dependency: ACF, especially get_field()
+*  http://www.advancedcustomfields.com/resources/functions/get_field/
+*/
+
+$extraArgs = [];
+$query = createQuery($wp_query, "tmt-coupon-posts", $extraArgs);
+
+// The Loop
+$rows = "";
+if ( $query->have_posts() ) {
+	while ( $query->have_posts() ) {
+ 		$query->the_post();
+		$id = get_the_ID();
+		$title = truncate(get_the_title(), 20);
+		$url = get_post_meta( $id, "url", true );
+		$merchant = get_the_term_list( $id, "merchant", $before, $sep, $after );
+		$code = get_field("code");
+		$expires = get_field("expires");
+		
+		$rows .= "<tr id='coupon-tr-$id'>";
+		$rows .= "<td><span>$merchant</span></td>";
+		$rows .= "<td>$title</td>";
+		$rows .= "<td>$code</td>";
+		$rows .= "<td>$expires</td>";
+		$rows .= "<td style='text-align:center'><button class='btn btn-xs btn-success'>View More</button></td>";
+		$rows .= "</tr>";
+ 	}
+}
+
+function truncate($string, $length) {
+	return substr($string, 0, strpos(wordwrap($string, $length), "\n")) . "...";
+}
+
+?>
+
+
+
 <div class="panel panel-default" style="height: 350px;">
 	<div class="panel-heading left quickOffersHeader" id="quickOffersHeader">
 		<h3 class="panel-title"><span>QUICK OFFERS</span><span title="View all your favorites">View all your favorite stores and coupons at a glance</span></h3>
@@ -31,69 +72,7 @@
 			</tr>
 		</thead>
 		<tbody>
-			<tr>
-				<td><span>Macys.com</span></td>
-				<td>Lowest Prices of the...</td>
-				<td>2BCGEXX</td>
-				<td>12/14</td>
-				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-			</tr>
-			<tr>
-				<td><span>Macys.com</span></td>
-				<td>Lowest Prices of the...</td>
-				<td>2BCGEXX</td>
-				<td>12/14</td>
-				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-			</tr>
-			<tr>
-				<td><span>Macys.com</span></td>
-				<td>Lowest Prices of the...</td>
-				<td>2BCGEXX</td>
-				<td>12/14</td>
-				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-			</tr>
-			<tr>
-				<td><span>Macys.com</span></td>
-				<td>Lowest Prices of the...</td>
-				<td>2BCGEXX</td>
-				<td>12/14</td>
-				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-			</tr>
-			<tr>
-				<td><span>Macys.com</span></td>
-				<td>Lowest Prices of the...</td>
-				<td>2BCGEXX</td>
-				<td>12/14</td>
-				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-			</tr>
-			<tr>
-				<td><span>Macys.com</span></td>
-				<td>Lowest Prices of the...</td>
-				<td>2BCGEXX</td>
-				<td>12/14</td>
-				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-			</tr>
-			<tr>
-				<td><span>Macys.com</span></td>
-				<td>Lowest Prices of the...</td>
-				<td>2BCGEXX</td>
-				<td>12/14</td>
-				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-			</tr>
-			<tr>
-				<td><span>Macys.com</span></td>
-				<td>Lowest Prices of the...</td>
-				<td>2BCGEXX</td>
-				<td>12/14</td>
-				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-			</tr>
-			<tr>
-				<td><span>Macys.com</span></td>
-				<td>Lowest Prices of the...</td>
-				<td>2BCGEXX</td>
-				<td>12/14</td>
-				<td style="text-align:center"><button class="btn btn-xs btn-success">View More</button></td>
-			</tr>
+			<?php echo $rows; ?>
 		</tbody>
 	</table>
 </div>
